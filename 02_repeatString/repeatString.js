c4f5ea770de30173d71980c64406d44f95dd39ee
andoss <anditahiri460@gmail.com>
2024-03-02 13:12:12 +0100

    Completed 1st and 2nd JS tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num < 0){
+        return "ERROR"
+    }else if(num == 0){
+        return ''
+    } 
+    else {
+        let str = string;
+        for(let i=0; i<num-1;i++){
+            str+=string;
+        }
+        return str;
+    }
 };
+repeatString('hey',3);
 
 // Do not edit below this line
 module.exports = repeatString;
